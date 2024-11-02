22f6cd6f765917622616ae6eb4e0e40deea58dd2
alexKamo <sashakamor@gmail.com>
2024-11-02 10:50:10 -0500

    testEmployee

---
+++ src/test/java/com/luv2code/springboot/thymeleafdemo/EmployeeServiceImplTest.java
@@ -0,0 +1,83 @@
+package com.luv2code.springboot.thymeleafdemo;
+
+import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+public class EmployeeServiceImplTest {
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    // EmployeeServiceImpl employeeService = new EmployeeServiceImpl(employeeRepository);
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    // Test findAll()
+    @Test
+    void testFindAll_ShouldReturnAllEmployeesOrderedByLastName() {
+        // Given
+        Employee employee1 = new Employee("John", "Doe", "john.doe@example.com");
+        Employee employee2 = new Employee("Jane", "Smith", "jane.smith@example.com");
+        Employee employee3 = new Employee("Aaa", "Abc","aaa.abc@gmail.com");
+
+        when(employeeRepository.findAllByOrderByLastNameAsc()).thenReturn(Arrays.asList(employee1, employee2));
+
+        // When
+        List<Employee> employees = employeeService.findAll();
+
+        // Then
+        assertEquals(3, employees.size(), "The employee list size should be 2");
+        assertEquals("Abc", employees.get(0).getLastName(), "The first employee's last name should be Abc");
+        assertEquals("Doe", employees.get(1).getLastName(), "The first employee's last name should be Doe");
+        assertEquals("Smith", employees.get(2).getLastName(), "The second employee's last name should be Smith");
+    }
+    // test findById
+
+    @Test
+    void testFindById_ShouldReturnEmployeeWhenExists(){
+
+        //given
+        int id = 1;
+        Employee employee = new Employee(id,"Doe","John", "john.doe@gmail.com");
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(employee));
+
+        //when
+        Employee result = employeeService.findById(id);
+
+        //then
+        assertNotNull(result, "The returned employee shouldn't be NULL");
+
+    }
+
+    // test findById when exc thrown
+
+    // test saveEmployee
+
+    // test for deleteEmp
+}
+
+
+
+
+
+
+
+
